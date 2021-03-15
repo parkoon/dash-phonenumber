ff15837e8a539c9dbe9677aed367ae87b9f6c175
parkoon <devparkoon@gmail.com>
2021-03-15 12:29:17 +0900

    feat: implement main func

---
+++ dash-phonenumber.ts
@@ -0,0 +1,29 @@
+function dashPhoneNumber(str: string): string {
+  const number = str.replace(/[^0-9]/g, "");
+  const length = number.length;
+
+  if (length < 4) return number;
+
+  let phone = "";
+
+  if (number.length < 7) {
+    phone += number.substr(0, 3);
+    phone += "-";
+    phone += number.substr(3);
+  } else if (number.length < 11) {
+    phone += number.substr(0, 3);
+    phone += "-";
+    phone += number.substr(3, 3);
+    phone += "-";
+    phone += number.substr(6);
+  } else {
+    phone += number.substr(0, 3);
+    phone += "-";
+    phone += number.substr(3, 4);
+    phone += "-";
+    phone += number.substr(7);
+  }
+  return phone;
+}
+
+export default dashPhoneNumber;
