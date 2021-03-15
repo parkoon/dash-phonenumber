864e7cec0122e790eaecee2abe4931856f4d9e04
parkoon <devparkoon@gmail.com>
2021-03-15 12:39:25 +0900

    chore: prettier

--- dash-phonenumber.ts
+++ dash-phonenumber.ts
@@ -1,29 +1,29 @@
 function dashPhoneNumber(str: string): string {
-  const number = str.replace(/[^0-9]/g, "");
-  const length = number.length;
+  const number = str.replace(/[^0-9]/g, '')
+  const length = number.length
 
-  if (length < 4) return number;
+  if (length < 4) return number
 
-  let phone = "";
+  let phone = ''
 
   if (number.length < 7) {
-    phone += number.substr(0, 3);
-    phone += "-";
-    phone += number.substr(3);
+    phone += number.substr(0, 3)
+    phone += '-'
+    phone += number.substr(3)
   } else if (number.length < 11) {
-    phone += number.substr(0, 3);
-    phone += "-";
-    phone += number.substr(3, 3);
-    phone += "-";
-    phone += number.substr(6);
+    phone += number.substr(0, 3)
+    phone += '-'
+    phone += number.substr(3, 3)
+    phone += '-'
+    phone += number.substr(6)
   } else {
-    phone += number.substr(0, 3);
-    phone += "-";
-    phone += number.substr(3, 4);
-    phone += "-";
-    phone += number.substr(7);
+    phone += number.substr(0, 3)
+    phone += '-'
+    phone += number.substr(3, 4)
+    phone += '-'
+    phone += number.substr(7)
   }
-  return phone;
+  return phone
 }
 
-export default dashPhoneNumber;
+export default { dashPhoneNumber }
