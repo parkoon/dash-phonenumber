434dbb2cd665f874818cb75dab3e9ea24d30ebee
parkoon <devparkoon@gmail.com>
2021-03-15 12:29:38 +0900

    chore: build result

---
+++ build/dash-phonenumber.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function dashPhoneNumber(str) {
+    var number = str.replace(/[^0-9]/g, "");
+    var length = number.length;
+    if (length < 4)
+        return number;
+    var phone = "";
+    if (number.length < 7) {
+        phone += number.substr(0, 3);
+        phone += "-";
+        phone += number.substr(3);
+    }
+    else if (number.length < 11) {
+        phone += number.substr(0, 3);
+        phone += "-";
+        phone += number.substr(3, 3);
+        phone += "-";
+        phone += number.substr(6);
+    }
+    else {
+        phone += number.substr(0, 3);
+        phone += "-";
+        phone += number.substr(3, 4);
+        phone += "-";
+        phone += number.substr(7);
+    }
+    return phone;
+}
+exports.default = dashPhoneNumber;
