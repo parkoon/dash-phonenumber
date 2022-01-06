b814eacded96ba8a4c03f062a389bdc67fc591f5
jayden <jayden@kcd.co.kr>
2022-01-06 16:12:49 +0900

    fix: **** 과 같이 마스킹 값이 들어올 경우 예외 처리

--- sample.js
+++ sample.js
@@ -1,4 +1,5 @@
 const dashPhoneNumber = require('./lib/dash-phonenumber').default
 
 console.log(dashPhoneNumber('01033280917')) // Result: 010-3328-0917
+console.log(dashPhoneNumber('0103328****')) // Result: 010-3328-0917
 console.log(dashPhoneNumber('01033280917', { separator: '. ' })) // Result: 010. 3328. 0917
--- src/dash-phonenumber.ts
+++ src/dash-phonenumber.ts
@@ -5,29 +5,28 @@ export type Option = {
 function dashPhoneNumber(str: string, option: Option = {}) {
   if (!option.separator) option.separator = '-'
 
-  const number = str.replace(/[^0-9]/g, '')
-  const length = number.length
+  const length = str.length
 
-  if (length < 4) return number
+  if (length < 4) return str
 
   let phone = ''
 
-  if (number.length < 7) {
-    phone += number.substr(0, 3)
+  if (str.length < 7) {
+    phone += str.substr(0, 3)
     phone += option.separator
-    phone += number.substr(3)
-  } else if (number.length < 11) {
-    phone += number.substr(0, 3)
+    phone += str.substr(3)
+  } else if (str.length < 11) {
+    phone += str.substr(0, 3)
     phone += option.separator
-    phone += number.substr(3, 3)
+    phone += str.substr(3, 3)
     phone += option.separator
-    phone += number.substr(6)
+    phone += str.substr(6)
   } else {
-    phone += number.substr(0, 3)
+    phone += str.substr(0, 3)
     phone += option.separator
-    phone += number.substr(3, 4)
+    phone += str.substr(3, 4)
     phone += option.separator
-    phone += number.substr(7)
+    phone += str.substr(7)
   }
   return phone
 }
